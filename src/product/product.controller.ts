c21047cb9ac76caa478eac3dd0e7b46f18972c65
Naphat <naphat.d@gmail.com>
2024-11-28 20:55:35 +0700

    nest-api update

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -16,13 +16,18 @@ export class ProductController {
   findAll() {
     return [];
   }
-  @Get('/date') // localhost:4000/api/v1/product
+  @Get('/date') // localhost:4000/api/v1/product/date
   getDate() {
+    // throw new HttpException('เกิดข้อผิดพลาด', HttpStatus.BAD_REQUEST);
     return { server_date: this.utilityService.getServerDate() };
   }
   @Version('2')
   @Get('/thai') // localhost:4000/api/v2/product
   getThaiDate() {
+    // throw new BadRequestException('เกิดข้อผิดพลาด', {
+    //   cause: new Error(),
+    //   description: 'แสดงข้อมูลไม่ได้',
+    // });
     return { server_date: this.globalHelperService.getServerThaiDate() };
   }
 }
