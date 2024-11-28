e18e36423391844eee3a918f59391bc9b0c71da4
Naphat <naphat.d@gmail.com>
2024-11-28 11:26:22 +0700

    nest-api update

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,9 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { UtilityService } from 'src/shared/utility/utility.service';
 
 @Controller('product')
 export class ProductController {
+  constructor(private readonly utilityService: UtilityService) {}
+
   @Get('/') // localhost:4000/product
   findAll() {
     return [];
+  }
+  @Get('/date') // localhost:4000/product
+  getDate() {
+    return { server_date: this.utilityService.getServerDate() };
   }
 }
