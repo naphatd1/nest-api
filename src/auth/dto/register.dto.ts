606c2a129f7b6030c30d8a4ad1fad6f878ffdb7a
Naphat <naphat.d@gmail.com>
2024-12-01 16:37:32 +0700

    nest-api update

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class RegisterDto {
+  @IsNotEmpty({ message: 'ชื่อ-สกุล ห้ามว่าง' })
+  fulname: string;
+
+  @IsNotEmpty({ message: 'อีเมล์ ห้ามว่าง' })
+  @IsEmail({}, { message: 'รูปแบบอีเมล์ไม่ถูกต้อง' })
+  email: string;
+
+  @IsNotEmpty({ message: 'รหัสผ่าน ห้ามว่าง' })
+  password: string;
+}
